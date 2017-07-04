69879a5eedd140ca2e7e46be633f391805fc8258
aidar <aibariev@fix.ru>
2017-07-04 12:13:57 +0300

    Dockerfile for 1.1.0 version

---
+++ 1.1.0/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Generate the config only if it doesn't exist
+mkdir conf
+if [ ! -f "conf/zk-web-conf.clj" ]; then
+    CONFIG="conf/zk-web-conf.clj"
+
+    printf "{ \n" >> $CONFIG
+    printf ":server-port $ZKWEB_PORT \n" >> $CONFIG
+    printf ":users {`echo $ZKWEB_CREDENTIALS | tr : ' ' | sed 's/[^ ][^ ]*/"&"/g'`} \n" >> $CONFIG
+    printf ":default-node \"$ZKWEB_DEFAULT_NODE\" \n" >> $CONFIG
+    printf "}" >> $CONFIG
+
+fi
+
+exec "$@"
